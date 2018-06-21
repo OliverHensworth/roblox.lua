efb122f95236d2ac047b847737c146a38945cb1a
Oliver Hensworth <oliverhensworth1@gmail.com>
2018-06-21 21:02:44 +0100

    Rewrite.

---
+++ endpoints.lua
@@ -0,0 +1,6 @@
+return {
+    groupInfo = "https://groups.roblox.com/v1/groups/%s",
+
+    userId = "https://api.roblox.com/users/%s",
+    username = "https://api.roblox.com/users/get-by-username?username=%s"
+}
