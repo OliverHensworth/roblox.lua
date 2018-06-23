987eacaa4f9ad03524e9248fde76290419291bfd
Oliver Hensworth <oliverhensworth1@gmail.com>
2018-06-23 14:51:36 +0100

    Added new containers.

--- init.lua
+++ init.lua
@@ -1,5 +1,7 @@
 return {
     group = require("./containers/groups"),
      user = require("./containers/users"),
-     misc = require("./containers/misc")
+     misc = require("./containers/misc"),
+     game = require("./containers/games"),
+    asset = require("./containers/assets")
 }
