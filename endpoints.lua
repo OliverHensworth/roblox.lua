0591ab8b519863ede41f17a1d86de7965e963910
Oliver Hensworth <oliverhensworth1@gmail.com>
2018-06-23 14:50:30 +0100

    Added more endpoints for the next committs.

--- endpoints.lua
+++ endpoints.lua
@@ -2,5 +2,9 @@ return {
     groupInfo = "https://groups.roblox.com/v1/groups/%s",
 
     userId = "https://api.roblox.com/users/%s",
-    username = "https://api.roblox.com/users/get-by-username?username=%s"
+    username = "https://api.roblox.com/users/get-by-username?username=%s",
+
+    gameInfo = "https://games.roblox.com/v1/games/%s",
+
+    assetInfo = "https://api.roblox.com/marketplace/productinfo?assetId=%s"
 }
